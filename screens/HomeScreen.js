1bd17aab7185058e71bc2a7461ad4696886266d6
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-07-26 23:35:59 +0700

    feat: success group tokens based on collections

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   Text,
   View,
+  FlatList,
 } from 'react-native';
 import axios from 'axios';
 
@@ -13,22 +14,47 @@ const HomeScreen = () => {
   const baseUrl = `https://api-generator.retool.com/jlEsLB/`;
   const [tokenCollections, setTokensCollections] = useState([]);
   const [isLoadingFinish, setIsLoadingFinish] = useState(false);
+  const [collectionList, setCollectionList] = useState([]);
+  const [collectionIdList, setCollectionIdList] = useState([]);
 
   const getAllTokens = async () => {
     const response = await axios.get(`${baseUrl}/wallet_content`);
     setTokensCollections(response.data);
+
+    tokenCollections.forEach(token => {
+      const parsingCollection = JSON.parse(token.collection_json);
+
+      if (!collectionIdList.includes(parsingCollection.id)) {
+        const oldIdCollections = collectionIdList;
+        oldIdCollections.push(parsingCollection.id);
+        setCollectionIdList(oldIdCollections);
+
+        const oldCollections = collectionList;
+        oldCollections.push(parsingCollection);
+        setCollectionList(oldCollections);
+      }
+    });
+
     setIsLoadingFinish(true);
   };
 
-  console.log(tokenCollections[4]?.name);
-
+  console.log(collectionList.length);
   useEffect(() => {
     getAllTokens();
   }, []);
 
+  const renderItem = ({item}) => <Text>{item.name}</Text>;
+
   return (
     <View>
-      <Text>Ini Main Page</Text>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        numColumns={3}
+        data={collectionList}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        contentContainerStyle={{paddingBottom: 400}}
+      />
     </View>
   );
 };
