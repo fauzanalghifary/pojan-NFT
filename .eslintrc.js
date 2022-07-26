ac1cb025c5571d3c0eace12c724404aa2d928573
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-07-26 22:02:36 +0700

    feat: clean app.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rule: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
