ae489cb4e213a117ddfe4242d3de283018f6d499
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-07-26 23:01:33 +0700

    feat: success call API using axios

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -7,8 +7,25 @@ import {
   Text,
   View,
 } from 'react-native';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const baseUrl = `https://api-generator.retool.com/jlEsLB/`;
+  const [tokenCollections, setTokensCollections] = useState([]);
+  const [isLoadingFinish, setIsLoadingFinish] = useState(false);
+
+  const getAllTokens = async () => {
+    const response = await axios.get(`${baseUrl}/wallet_content`);
+    setTokensCollections(response.data);
+    setIsLoadingFinish(true);
+  };
+
+  console.log(tokenCollections[4]?.name);
+
+  useEffect(() => {
+    getAllTokens();
+  }, []);
+
   return (
     <View>
       <Text>Ini Main Page</Text>
